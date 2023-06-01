8c0556fbb99947404d31026fabee3c9c17552f3d
Robert Nathan Moraes <52546440+robertnathanmoraes@users.noreply.github.com>
2023-06-01 13:11:42 -0300

    layout formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calc';
+  submitForm: any;
+  grauDependencia = [
+    {
+      nome: 'Cônjuge e companheiro(a)',
+      value: 'Cônjuge e companheiro(a)'
+    },
+    {
+      nome: 'Dependente sob condição de invalidez',
+      value: 'Dependente sob condição de invalidez',
+    },
+    {
+      nome: 'Enteado(a)',
+      value: 'Enteado(a)',
+    },
+    {
+      nome: 'Filho(a)',
+      value: 'Filho(a)',
+    },
+    {
+      nome: 'Ex-cônjuge ou Ex-convivente com pensão alimenticia',
+      value: 'Ex-cônjuge ou Ex-convivente com pensão alimenticia',
+    },
+    {
+      nome: 'Menor sob guarda',
+      value: 'Menor sob guarda',
+    },
+    {
+      nome: 'Tutelado(a)',
+      value: 'Tutelado(a)'
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTableModule} from "@angular/material/table";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
