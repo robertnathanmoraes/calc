f3a61a976cf103d87b103c3165fe79fa59bee6da
Robert Nathan Moraes <52546440+robertnathanmoraes@users.noreply.github.com>
2023-06-07 16:25:36 -0300

    layout formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 // @ts-ignore
 import faixasEtariasJSON from '../assets/formula.json';
-import {FormArray, FormBuilder, Validators} from "@angular/forms";
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
+  faixasEtarias: FaixaEtaria[];
+  resultados: { faixaIdade: any; valorCalculo: any }[] = [];
+  salario: string = '';
+  idade: any = '';
+  resultado: string = '';
+  submitForm: any;
+  faixaIdade: string | undefined
   grauDependencia = [
     {
       nome: 'Cônjuge e companheiro(a)',
@@ -40,13 +46,10 @@ export class AppComponent {
       value: 'Tutelado(a)'
     },
   ]
-  salario: string = '';
-  idade: any = '';
-  resultado: string = '';
-  submitForm: any;
-  faixaIdade: string | undefined
 
   constructor(private formBuilder: FormBuilder) {
+    // Carregar faixas etárias do JSON
+    this.faixasEtarias = faixasEtariasJSON.faixas;
     this.submitForm = this.formBuilder.group({
       salarioContribuicao: [null, Validators.required],
       idade: [null, Validators.required],
@@ -55,7 +58,6 @@ export class AppComponent {
       numeroDependentes: [''],
       dependentes: this.formBuilder.array([])
     });
-
   }
 
   adicionarDependentes() {
@@ -87,31 +89,105 @@ export class AppComponent {
   }
 
   loadFaixasEtarias() {
-    const faixasEtarias: FaixaEtaria[] = faixasEtariasJSON.faixas;
-    this.resultado = this.calcularTetoPorcentagem(faixasEtarias).toFixed(2);
+    if (!faixasEtariasJSON || !faixasEtariasJSON.faixas) {
+      console.log('Faixas etárias não carregadas corretamente');
+      return;
+    }
+
+    this.faixasEtarias = faixasEtariasJSON.faixas;
+
+    // Calcular resultados para todas as faixas etárias
+    this.resultados = this.faixasEtarias.map((faixa) => {
+      return {
+        faixaIdade: this.getIdadeFaixa(faixa),
+        valorCalculo: this.calcularTetoPorcentagem(faixa),
+      };
+    });
   }
 
-  calcularTetoPorcentagem(faixasEtarias: FaixaEtaria[]): number {
+  getIdadeFaixa(faixa: FaixaEtaria): string {
+    if (faixa.idadeMax >= 60) {
+      return 'Acima de 59 anos';
+    } else {
+      return faixa.idadeMin + '-' + faixa.idadeMax;
+    }
+  }
+
+  calcularTetoPorcentagem(faixa: FaixaEtaria): number {
     const salario = parseFloat(this.submitForm.value.salarioContribuicao);
     const idade = parseFloat(this.submitForm.value.idade);
 
-    for (let i = 0; i < faixasEtarias.length; i++) {
-      const faixa = faixasEtarias[i];
-
-      if (idade >= faixa.idadeMin && idade <= faixa.idadeMax) {
-        const valorCalculo = (salario * faixa.porcentagem) / 100;
-        this.faixaIdade = ((faixa.idadeMax >= 60 ? "Acima de 59 anos" : faixa.idadeMin + '-' + faixa.idadeMax) )
-        this.idade  = idade
-        return Math.min(valorCalculo, faixa.teto);
-      }
+    if (idade >= faixa.idadeMin && idade <= faixa.idadeMax) {
+      return Math.min((salario * faixa.porcentagem) / 100, faixa.teto);
     }
 
-    return 0; // Faixa etária não encontrada
+    return 0;
+  }
+
+
+  calcularCenario1(salario: number, idade: number) {
+    // Cálculo 1: 3.10% sobre o salário
+    const valor = (salario * 3.1) / 100;
+    const faixaIdade = this.obterFaixaIdade(idade);
+    // @ts-ignore
+    this.resultados.push({ salario, idade, faixaIdade, valor });
+  }
+
+  calcularCenario2(salario: any, idade: number) {
+    // Cálculo 2: 3.60% sobre o salário, adicionando o valor sobre cada dependente conforme idade,
+    // podendo atingir o valor máximo de 12% do salário do funcionário
+    const valorBase = (salario * 3.6) / 100;
+    const valorDependentes = this.calcularValorDependentes(idade);
+    const valor = Math.min(valorBase + valorDependentes, (salario * 12) / 100);
+    const faixaIdade = this.obterFaixaIdade(idade);
+    // @ts-ignore
+    this.resultados.push({ salario, idade, faixaIdade, valor });
+  }
+
+  calcularCenario3(salario: number) {
+    // Cálculo 3: 60% do salário + salário
+    const valor = (salario * 60) / 100 + salario;
+    // @ts-ignore
+    this.resultados.push({ salario, idade: null, faixaIdade: null, valor });
+  }
+
+  calcularCenario4(salario: number) {
+    // Cálculo 4: 3.10% do salário do cálculo 3
+    const valorCenario3 = (salario * 60) / 100 + salario;
+    const valor = (valorCenario3 * 3.1) / 100;
+    // @ts-ignore
+    this.resultados.push({ salario, idade: null, faixaIdade: null, valor });
+  }
+
+  // Outros métodos de cálculo...
+
+  obterFaixaIdade(idade: number): string {
+    for (const faixa of this.faixasEtarias) {
+      if (idade >= faixa.idadeMin && idade <= faixa.idadeMax) {
+        return faixa.idadeMin + '-' + faixa.idadeMax;
+      }
+    }
+    return 'Faixa etária não encontrada';
+  }
+
+  calcularValorDependentes(idade: number): number {
+    // Lógica para calcular o valor dos dependentes com base na idade
+    // ...
+
+    return 0; // Valor calculado dos dependentes
   }
 }
+
 interface FaixaEtaria {
   idadeMin: number;
   idadeMax: number;
   teto: number;
   porcentagem: number;
+}
+
+interface ResultadoCalculo {
+  salario: number;
+  idade: number | null;
+  faixaIdade: string | null;
+  valor: number;
 }
