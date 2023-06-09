a8de0653db968d9f1da18ca8937e9cb2d4269e8d
Robert <robert.nunes@evo1.com.br>
2023-06-09 02:35:33 -0300

    layout mais calculos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
   calcular2: number = 0;
   calcular3: number = 0;
   calcular4: number = 0;
-  totalDependente: number | undefined;
+  totalDependente: string | undefined;
   grauDependencia = [
     {
       nome: 'Cônjuge e companheiro(a)',
@@ -43,19 +43,52 @@ export class AppComponent {
       value: 'Tutelado(a)'
     },
   ]
+  valorTitular: string | undefined;
 
   constructor(private formBuilder: FormBuilder) {
     // Carregar faixas etárias do JSON
     this.faixasEtarias = faixasEtariasJSON.faixas;
     this.submitForm = this.formBuilder.group({
       salarioContribuicao: [null, Validators.required],
-      idade: [null, Validators.required],
+      idade: [''],
       grauDependencia: [''],
       dependenteIdade: [''],
       numeroDependentes: [''],
       dependentes: this.formBuilder.array([])
     });
   }
+
+  transform(value: string): string {
+    const primeiroEspaco = value.indexOf(' ');
+    if (primeiroEspaco !== -1) {
+      return value.substring(0, primeiroEspaco);
+    }
+
+    return value;
+  }
+
+  public validateInputValue(event: KeyboardEvent): boolean {
+    const inputChar = event.key;
+
+    // Verifica se o caractere digitado é um caracter especial
+    if (this.isSpecialCharacter(inputChar)) {
+      event.preventDefault();
+      return false;
+    }
+
+    return true;
+  }
+
+  private isSpecialCharacter(char: string): boolean {
+    const regex = /[^0-9\b]/g;
+    // @ts-ignore
+    if (event.key === 'Backspace') {
+      return false; // Permite que a tecla "Backspace" seja usada para apagar
+    } else {
+      return regex.test(char);
+    }
+  }
+
 
   formatCurrency(value: number): string {
     const formatter = new Intl.NumberFormat('pt-BR', {
@@ -104,13 +137,6 @@ export class AppComponent {
     const salario = parseFloat(this.submitForm.value.salarioContribuicao);
     const idade = parseFloat(this.submitForm.value.idade);
 
-    // Calcular resultados para todas as faixas etárias
-    this.resultados = this.faixasEtarias.map((faixa) => {
-      return {
-        faixaIdade: this.getIdadeFaixa(faixa),
-        valorCalculo: this.calcularTetoPorcentagem(faixa),
-      };
-    });
 
     // @ts-ignore
     this.calcular1 = this.calcularCenario1(salario);
@@ -121,26 +147,6 @@ export class AppComponent {
     // @ts-ignore
     this.calcular4 = this.calcularCenario4(salario);
   }
-
-  getIdadeFaixa(faixa: FaixaEtaria): string {
-    if (faixa.idadeMax >= 60) {
-      return 'Acima de 59 anos';
-    } else {
-      return faixa.idadeMin + '-' + faixa.idadeMax;
-    }
-  }
-
-  calcularTetoPorcentagem(faixa: FaixaEtaria): number {
-    const salario = parseFloat(this.submitForm.value.salarioContribuicao);
-    const idade = parseFloat(this.submitForm.value.idade);
-
-    if (idade >= faixa.idadeMin && idade <= faixa.idadeMax) {
-      return Math.min((salario * faixa.porcentagem) / 100, faixa.teto);
-    }
-
-    return 0;
-  }
-
 
   calcularCenario1(salario: number, idade: number) {
     // Cálculo 1: 3.10% sobre o salário
@@ -154,10 +160,11 @@ export class AppComponent {
   calcularCenario2(salario: any, idade: number) {
     // Cálculo 2: 3.60% sobre o salário, adicionando o valor sobre cada dependente conforme tabela de valores por idade.,
     // podendo atingir o valor máximo de 12% do salário do funcionário
-
-    const valorBase = (salario * 3.6) / 100;
+    this.valorTitular = this.formatCurrency((salario * 3.6) / 100);
+    const valorTitular = (salario * 3.6) / 100
     const valorDependentes = this.calcularValorDependentes(idade);
-    const valor = Math.min((valorBase + valorDependentes), (salario * 12) / 100);
+    const valor = Math.min(Number((valorTitular + valorDependentes)), (salario * 12) / 100);
+
     // @ts-ignore
     return this.formatCurrency(valor)
 
@@ -186,22 +193,35 @@ export class AppComponent {
 
   obterFaixaIdade(idade: number): string {
     for (const faixa of this.faixasEtarias) {
+
+
+      if (idade >= 60) {
+        return 'Acima de 59'
+      }
       if (idade >= faixa.idadeMin && idade <= faixa.idadeMax) {
-        return faixa.idadeMin + '-' + faixa.idadeMax;
+        return faixa.idadeMin + ' - ' + faixa.idadeMax;
       }
     }
     return 'Faixa etária não encontrada';
   }
 
+  obterValorDaFaixaDeIdade(idade: number): any {
+    for (const faixa of this.faixasEtarias) {
+      if (idade >= faixa.idadeMin && idade <= faixa.idadeMax) {
+        return faixa.valor;
+      }
+    }
+
+    return null;
+  }
+
   calcularValorDependentes(idade: number): number {
-    console.log('idade', idade)
     const dependentes = this.submitForm.get('dependentes') as FormArray; //pega todos depententes
     let valorTotalDependentes = 0;
 
     dependentes.controls.forEach((dependenteGroup: any) => { //passa por cada dependente
       const grauDependencia = dependenteGroup.value.grauDependencia; //nome do tipo de dependente
       const dependenteIdade = parseFloat(dependenteGroup.value.dependenteIdade); //idade do dependente
-      console.log('dependenteIdade', dependenteIdade)
 
       let dependenteValor = 0;
 
@@ -212,7 +232,6 @@ export class AppComponent {
         //retorna true e pega o item.
         return dependenteIdade >= valor.idadeMin && (valor.idadeMax === null || dependenteIdade <= valor.idadeMax);
       });
-      console.log('valorDependente', valorDependente)
 
       if (valorDependente) {
         dependenteValor = valorDependente.valor;
@@ -220,16 +239,11 @@ export class AppComponent {
 
       // Adicionar o valor do dependente ao total
       valorTotalDependentes += dependenteValor;
-      console.log('dependenteValor', dependenteValor)
 
     });
 
     // Somar o valor dos dependentes ao valor base (3.60% do salário)
-    this.totalDependente = valorTotalDependentes
-
-    const valorBase = (this.submitForm.value.salarioContribuicao * 3.6) / 100;
-
-    // const valorTotal = valorBase + valorTotalDependentes;
+    this.totalDependente = this.formatCurrency(valorTotalDependentes)
 
     // Verificar se o valor total ultrapassa o teto de 12% do salário
     return valorTotalDependentes;
@@ -240,7 +254,7 @@ interface FaixaEtaria {
   idadeMin: number;
   idadeMax: number;
   teto: number;
-  porcentagem: number;
+  valor: number;
 }
 
 interface ResultadoCalculo {
